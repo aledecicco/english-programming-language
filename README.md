3252bb97709c2a1819be215737260a3723a8d168
Alejandro De Cicco <alejandrodecicco99@gmail.com>
2021-09-13 23:48:07 -0300

    Command line arguments

--- README.md
+++ README.md
@@ -1,19 +1,25 @@
 # EPL
 A programming language that resembles English. Write some sentences and watch the interpreter try to understand what you meant.
 
-![Version](https://img.shields.io/badge/version-v3.9.0-blue)
+![Version](https://img.shields.io/badge/version-v3.10.0-blue)
 ![License](https://img.shields.io/badge/license-MIT-brightgreen)
 
 # Introduction
-The goal of this project is to make a language that is as similar as possible to plain English. Natural language is ambiguous and hard to understand for computers, so we apply a series of rules and constraints to it. With them, we end up with a language that looks like English
+The goal of this project is to make a language that is as similar as possible to plain English. Natural language is ambiguous, so we apply a series of rules and constraints to it. With them, we end up with a language that looks like English, but that is stable enough to be understood by a computer.
 
-[Here](docs/syntax.md) is the documentation for the language's syntax.
+Here are some sentences that the interpreter can understand:
+```
+Let L be the
+Set
+```
+
+[Here](docs/syntax.md) is the full documentation for the language's syntax.
 
 [Here](docs/prelude.md) is the documentation for the language's prelude.
 
 [Here](examples) are some examples of EPL programs.
 
-# Running the interpreter
+# Usage
 Requirements: [Stack](https://docs.haskellstack.org/en/stable/README/)
 
 To run a program, for example `program.epl`, simply use:
@@ -21,15 +27,25 @@ To run a program, for example `program.epl`, simply use:
 stack run -- program.epl
 ```
 
+The `-h` or `--help` option displays usage help.
+
+The `-s` or `--silent` option runs the interpreter without printing warnings or success messages.
+
+# Features
+- Spaces are allowed in variable and function names.
+- Arguments can be passed by value or by reference.
+- Garbage collector.
+- Try-catch, loops and conditionals.
+- Optional function parameter names, with aliases generation.
 
 # Work in progress
 - Adding more built-in functions.
-- Adding "break" for loops and "exit" for procedures (stop the loop and exit?).
-- Implementing command line arguments for the interpreter.
+- Adding "break" for loops and "exit" for procedures ("stop the loop" and "exit"?).
+- Special variable "it" for referencing the result of the previous sentence.
 
 # Possible improvements:
-- Finding a better syntax for specifying function return types
-- Implementing importing other files as libraries
+- Finding a better syntax for specifying function return types.
+- Implementing importing other files as libraries.
 - Adding records to the language.
-
+- Implementing an interactive environment.
 
--- app/Main.hs
+++ app/Main.hs
@@ -9,7 +9,10 @@ The entry point of the interpreter.
 
 module Main where
 
+import Control.Monad (mapM_, unless, when)
 import System.Console.ANSI
+import System.Environment (getArgs)
+import System.Console.GetOpt
 import System.Exit (exitFailure, exitSuccess)
 import System.IO.Error (tryIOError)
 
@@ -18,6 +21,42 @@ import Evaluator (evaluateProgram)
 import FuzzyParser (parseProgram)
 import PrettyPrinter (ppError, ppWarning)
 import Solver (solveProgram)
+
+
+-- -----------------
+-- * Options
+
+-- | The flags that can be passed via command line.
+data Flag =
+    Silent -- ^ Don't output warning or success messages. `-s` or `--silent`.
+    | Help -- ^ Output a help message. `-h` or `--help`.
+    deriving Eq
+
+options :: [OptDescr Flag]
+options =
+    [
+        Option ['s'] ["silent"] (NoArg Silent) "Don't display messages. Only errors and program output are shown.",
+        Option ['h'] ["help"] (NoArg Help) "Display this message."
+    ]
+
+-- | The help message to show with the `-h` option.
+help :: String
+help = init $  usageInfo "Usage: stack run -- [-s] file" options
+
+-- | Returns the command line arguments for the interpreter.
+getOptions :: IO ([Flag], FilePath)
+getOptions = do
+    -- Get the arguments.
+    (opts, args, errs) <- getOpt Permute options <$> getArgs
+    -- If there are misused flags, print errors and exit.
+    unless (null errs) $ mapM_ (errorMessage . ppFlagError) errs >> exitFailure
+    -- If there isn't exactly one non-flag argument, print an error and exit.
+    when (length args /= 1) $ errorMessage "Error: expected one file as argument." >> exitFailure
+    return (opts, head args)
+    where
+        ppFlagError :: String -> String
+        ppFlagError err = "Error: " ++ init err ++ "."
+
 
 
 -- -----------------
@@ -40,9 +79,15 @@ warningMessage = message Yellow
 successMessage :: String -> IO ()
 successMessage = message Green
 
+commonMessage :: String -> IO ()
+commonMessage = message White
+
 -- | If a computation failed, prints the error message and exits.
 -- Otherwise, returns the result.
-tryOrExit :: Either Error a -> [String] -> IO a
+tryOrExit ::
+    Either Error a -- ^ The result or error of a computation.
+    -> [String] -- ^ The lines of the source code.
+    -> IO a -- ^ The result of the computation.
 tryOrExit (Left err) lines = do
     errorMessage $ ppError lines err
     exitFailure
@@ -58,21 +103,26 @@ main :: IO ()
 main = do
     putChar '\n'
 
+    -- Parse command line arguments.
+    (opts, filePath) <- getOptions
+    when (Help `elem` opts) $ commonMessage help >> exitSuccess
+    let silent = Silent `elem` opts
+
     -- Read the file contents.
-    readRes <- tryIOError $ readFile "examples/Head.epl"
-    fc <- case readRes of
+    readRes <- tryIOError $ readFile filePath
+    fileContent <- case readRes of
         Left _ -> errorMessage "Error: the input file could not be opened." >> exitFailure
         Right str -> return str
-    let fileLines = lines fc
+    let fileLines = lines fileContent
 
     -- Parse the program.
-    unsolvedProg <- tryOrExit (parseProgram fc) fileLines
+    unsolvedProg <- tryOrExit (parseProgram fileContent) fileLines
 
     -- Solve missing pieces.
     let (solverRes, ws) = solveProgram unsolvedProg
-    mapM_ (warningMessage . ppWarning fileLines) $ reverse ws
+    unless silent $ mapM_ (warningMessage . ppWarning fileLines) (reverse ws)
     ((solvedProg, _), _) <- tryOrExit solverRes fileLines
-    successMessage "Parsed successfully."
+    unless silent $ successMessage "Parsed successfully."
 
     -- Evaluate the result.
     evalRes <- evaluateProgram solvedProg
